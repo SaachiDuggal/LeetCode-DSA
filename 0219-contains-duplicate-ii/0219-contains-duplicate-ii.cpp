ec538608ce43960ea19030fae5a3bbe79868796e
SaachiDuggal <118597705+SaachiDuggal@users.noreply.github.com>
2026-06-13 12:29:24 +0530

    Time: 68 ms (83.98%), Space: 93.7 MB (88.8%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -2,29 +2,32 @@ class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
         
-        //edge case if array is empty or just have one element or if k == 0
-        if(nums.empty() || nums.size() == 1 || k == 0){
+        //edge case: if the array size is <= 1 or if k = 0
+        if(nums.size() <= 1 || k == 0){
             return false;
         }
 
-        //initialising a hash set for storing k elements
+        //initialising a set to store the values in the k window in the array
         unordered_set<int> window;
 
-        //looping through the array to find a duplicate within k size
+        //initialising variable for the leftMost index of the k window
+        int left = 0;
+
+        //looping the array with a right variable to move across the array and scan for duplicates in the k window
         for(int right = 0; right < nums.size(); right++){
 
-            //when the size of hash set exceeds the window of k
-            if(right > k){
-                //remove the leftMost element of the hashset to maintain the length
-                window.erase(nums[right - k - 1]);
+            //we check first if the right index is in the window or not
+            while(right - left > k){
+                window.erase(nums[left]);
+                left++;
             }
 
-            //if we find a match
+            //we check for the duplicate
             if(window.find(nums[right]) != window.end()){
                 return true;
             }
 
-            //else we just add the nums[right] in the set and move to the next element
+            //we insert the element
             window.insert(nums[right]);
         }
         return false;
