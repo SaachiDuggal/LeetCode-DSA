6bb2bb605dab9d7b931cc8c02be81e6ea8671706
SaachiDuggal <118597705+SaachiDuggal@users.noreply.github.com>
2026-08-16 00:26:32 +0530

    Time: 0 ms (100%), Space: 15.3 MB (36.78%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,3 +1,4 @@
+/*
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
@@ -41,4 +42,55 @@ public:
         }
         return -1;
     }
+};
+*/
+
+
+class Solution {
+public:
+
+    int binarySearch(vector<int>& nums, int target, int left, int right){
+
+        //base case
+        if(left > right){
+            return -1;
+        }
+
+        //calculate mid
+        int mid = left + (right - left) / 2;
+
+        //found
+        if(nums[mid] == target){
+            return mid;
+        }
+
+        //if Left side is sorted
+        if(nums[left] <= nums [mid]){
+
+            //if target lies in the sorted range
+            if(target < nums[mid] && target >= nums[left]){
+                return binarySearch(nums, target, left, mid - 1);
+            }
+            else{
+                return binarySearch(nums, target, mid + 1, right);
+            }
+        }
+
+        //if right side is sorted
+        else{
+
+            //if target lies in the sorted range
+            if(target > nums[mid] && target <= nums[right]){
+                return binarySearch(nums, target, mid + 1, right);
+            }
+            else{
+                return binarySearch(nums, target, left, mid - 1);
+            }
+        }
+    }
+
+    int search(vector<int>& nums, int target) {
+        return binarySearch(nums, target, 0, nums.size() - 1);
+    }
+
 };
